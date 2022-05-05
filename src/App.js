191ae54c639957a27bd7108dfb95d9a83af0b631
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-05-05 11:08:09 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import { ToastContainer } from "react-toastify";
 import './App.css';
+import Navbar from "./components/Navbar";
 
 const App =() => {
   return (
     <div className="App">
     <ToastContainer/>
+    <Navbar/>
     <h1>Welcome to react redux contact book</h1>
     </div>
   );
