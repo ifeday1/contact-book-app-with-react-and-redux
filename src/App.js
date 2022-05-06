ebd2f870a3be99bc9374b5b940aaf46cb58ba6ce
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2022-05-06 11:45:39 +0100

    React routing and add contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react"
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import './App.css';
+import AddContact from "./components/AddContact";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 const App =() => {
@@ -8,7 +12,18 @@ const App =() => {
     <div className="App">
     <ToastContainer/>
     <Navbar/>
-    <h1>Welcome to react redux contact book</h1>
+    <Switch>
+    <Route exact path="/" component={()=> <Home/> }></Route>
+ 
+    <Route  path="/add">
+    <AddContact/>
+    </Route>
+
+    <Route  path="/edit/:id">
+    <h1>I am a edit components</h1>
+    </Route>
+    </Switch>
+   
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark py-2' >
+    <Link to="/" className='navbar-brand ml-5'> React Redux Contact App</Link>
+    </nav>
   )
 }
 
